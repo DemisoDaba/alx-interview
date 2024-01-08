3d4d7f9b2c8d567ab95324ee141ce056c2f3dd7e
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-07 22:51:50 -0800

    fixxing some issues in

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,23 +7,24 @@ def canUnlockAll(boxes):
     if not boxes or len(boxes) == 1:
         return True
 
+    # Initiate keys, unlocked boxes, and track visited boxes
     keys = set(boxes[0])
     unlocked = {0}
     visited = set()
 
     while keys:
+
         new_keys = set()
         for box in unlocked - visited:
             for key in boxes[box]:
                 if key not in unlocked:
-                    new_keys.add(key)
+                    new_keys.add(key)  
                     unlocked.add(key)
-            visited.add(box)
-
+            visited.add(box)           
+        
         if not new_keys:
-            break
-
+            break                      
+        
         keys |= new_keys
 
     return len(unlocked) == len(boxes)
-
