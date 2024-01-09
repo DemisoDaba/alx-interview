673bc3537da324224c3815875fa2f4a342c55439
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-01-09 09:14:50 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,28 @@
 Checks if all the boxes in a list can be unlocked.
 """
 
-from collections import deque
+def canUnlockAll(boxes):
+    if not boxes or len(boxes) == 1:
+        return True
 
-def canUnlockAll(boxes_to_check):
-    total_boxes = len(boxes_to_check)
-    unlocked_boxes = [False] * total_boxes
-    unlocked_boxes[0] = True
-    queue = deque([0])
-    
-    while queue:
-        current_box = queue.popleft()
-        for key in boxes_to_check[current_box]:
-            if key < total_boxes and not unlocked_boxes[key]:
-                unlocked_boxes[key] = True
-                queue.append(key)
-                
-    return all(unlocked_boxes)
+    # Initiate keys, unlocked boxes, and track visited boxes
+    keys = set(boxes[0])
+    unlocked = {0}
+    visited = set()
+
+    while keys:
+
+        new_keys = set()
+        for box in unlocked - visited:
+            for key in boxes[box]:
+                if key not in unlocked:
+                    new_keys.add(key)
+                    unlocked.add(key)
+            visited.add(box)
+
+        if not new_keys:
+            break
+
+        keys |= new_keys
+
+    return len(unlocked) == len(boxes)
