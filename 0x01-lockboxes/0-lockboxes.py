197e43c9eec51d9abb1f17509f8ba1540678bde9
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-13 04:04:20 -0800

    Fixed and done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,11 +4,11 @@
 
 def canUnlockAll(boxes):
     """ LockBoxes Function """
-    T = []
+    box = []
     for i in range(1, len(boxes)):
-        order = [T.extend(x) for x in boxes[:i] + boxes[i + 1:]]
-        if i in T:
-            T = []
+        order = [box.extend(x) for x in boxes[:i] + boxes[i + 1:]]
+        if i in box:
+            box = []
         else:
             return False
     return True
