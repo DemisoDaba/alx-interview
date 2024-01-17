730a0c3efa7f6cf9d6be799e76c1f0800fc4d33b
DemisoDaba <demo.nkmt1@gmail.com>
2024-01-17 07:32:35 -0800

    Implement minOperations function

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,17 +2,17 @@
 """This module defines the function minOperations"""
 
 
-def minOperations(num_H):
+def minOperations(h):
     """This function takes as argument
-        num_H: number of H characters that should be printed
+        h: number of H characters that should be printed
         and returns the number of operations to achieve that"""
-    if not isinstance(num_H, int) or num_H <= 0:
+    if not isinstance(h, int) or h <= 0:
         return 0
     copy_var = 1
     rep_var = 1
     op_count = 0
-    while rep_var < num_H:
-        if num_H % rep_var == 0:
+    while rep_var < h:
+        if h % rep_var == 0:
             copy_var = rep_var
             rep_var *= 2
             op_count += 2
