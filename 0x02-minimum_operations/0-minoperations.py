50841a365c90416ddc9aa43267fc7589fdd450f1
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-01-17 18:26:56 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
+"""This module defines the function minOperations"""
 
 
-def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+def minOperations(num_H):
+    """This function takes as argument
+        num_H: number of H characters that should be printed
+        and returns the number of operations to achieve that"""
+    if not isinstance(num_H, int) or num_H <= 0:
         return 0
-    return op
+    copy_var = 1
+    rep_var = 1
+    op_count = 0
+    while rep_var < num_H:
+        if num_H % rep_var == 0:
+            copy_var = rep_var
+            rep_var *= 2
+            op_count += 2
+        else:
+            rep_var += copy_var
+            op_count += 1
+    return op_count
