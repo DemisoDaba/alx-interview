b3c4a1e35e8f1b2d0820ef8f1b2e65d3bc8eb10d
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-01-23 11:39:35 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,10 +5,9 @@ A script for parsing HTTP request logs.
 
 import re
 
-
 def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
+   """ Extracts sections of a line of an HTTP request log."""
+
     fp = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
@@ -31,8 +30,7 @@ def extract_input(input_line):
 
 
 def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
+    """ Prints the accumulated statistics of the HTTP request log."""
     print('File size: {:d}'.format(total_file_size), flush=True)
     for status_code in sorted(status_codes_stats.keys()):
         num = status_codes_stats.get(status_code, 0)
@@ -41,14 +39,7 @@ def print_statistics(total_file_size, status_codes_stats):
 
 
 def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
+    """ Updates the metrics from a given HTTP request log."""
     line_info = extract_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
@@ -57,8 +48,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
 
 
 def run():
-    '''Starts the log parser.
-    '''
+    """ Starts the log parser."""
     line_num = 0
     total_file_size = 0
     status_codes_stats = {
@@ -84,7 +74,6 @@ def run():
                 print_statistics(total_file_size, status_codes_stats)
     except (KeyboardInterrupt, EOFError):
         print_statistics(total_file_size, status_codes_stats)
-
 
 if __name__ == '__main__':
     run()
