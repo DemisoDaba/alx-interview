0bc3ea4a2e02d3820693ede4f83f38ae38c1ff30
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-01-29 17:00:51 +0300

    re-updating 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,24 +8,24 @@ def validUTF8(data):
     """
     Return: True if data is a valid UTF-8 encoding, else return False.
     """
-    n_bytes = 0
+    num_bytes = 0
     for num in data:
         bin_rep = format(num, '#010b')[-8:]
-        if n_bytes == 0:
+        if num_bytes == 0:
             for bit in bin_rep:
                 if bit == '0':
                     break
-                n_bytes += 1
+                num_bytes += 1
 
-            if n_bytes == 0:
+            if num_bytes == 0:
                 continue
 
-            if n_bytes == 1 or n_bytes > 4:
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
             if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
 
-        n_bytes -= 1
+        num_bytes -= 1
 
-    return n_bytes == 0
+    return num_bytes == 0
