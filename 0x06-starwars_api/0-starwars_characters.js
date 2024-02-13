011d0e666d7c667463d9ba02a5c5b38e826f9cc6
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-13 09:15:22 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,22 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
+const ID = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
-const filmId = process.argv[2];
-
-const URL = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
-
-axios.get(URL)
-  .then(response => {
-    const film = response.data;
-    const characters = film.characters;
-
-    const printCharacterName = async (characterUrl) => {
-      try {
-        const characterResponse = await axios.get(characterUrl);
-        console.log(characterResponse.data.name);
-      } catch (error) {
-        console.error('Error:', error.response.status);
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const film = JSON.parse(response.body);
+  const listChar = film.characters;
+  for (const i in listChar) {
+    request(`${listChar[i]}`, (error, response, body) => {
+      if (error) {
+        console.log(error);
       }
-    };
-
-    characters.forEach(printCharacterName);
-  })
-  .catch(error => {
-    console.error('Error:', error.response.status);
-  });
-
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+    });
+  }
+});
