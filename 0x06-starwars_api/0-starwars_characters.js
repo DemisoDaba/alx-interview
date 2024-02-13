af0c7fc1f3769132b6176c852820a8af6a5b9fc0
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-13 09:18:12 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,19 +4,27 @@ const request = require('request');
 const ID = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${ID}/`;
 
+function getCharacter (listChar, index) {
+  if (index === listChar.length) {
+    return;
+  }
+  request(`${listChar[index]}`, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const character = JSON.parse(response.body);
+      console.log(character.name);
+      getCharacter(listChar, index + 1);
+    }
+  });
+}
+
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  }
-  const film = JSON.parse(response.body);
-  const listChar = film.characters;
-  for (const i in listChar) {
-    request(`${listChar[i]}`, (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-      const character = JSON.parse(response.body);
-      console.log(character.name);
-    });
+  } else {
+    const film = JSON.parse(response.body);
+    const listChar = film.characters;
+    getCharacter(listChar, 0);
   }
 });
