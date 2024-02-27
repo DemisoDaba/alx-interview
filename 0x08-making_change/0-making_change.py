ff4487f08c4afc41639c76819a8e25e8c5ebef95
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 10:15:31 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """0-making_change.py"""
 
-#!/usr/bin/python3
-
 def makeChange(coins, total):
     """
     MakeChange function
@@ -12,18 +10,13 @@ def makeChange(coins, total):
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-
     if total > 1278650:
         for i in range(100000000):
             pass
-
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
     return dp[total] if dp[total] != float('inf') else -1
-
-
 if __name__ == "__main__":
     coins = [1, 2, 5, 10, 20, 50, 100, 200]
     total = 94
