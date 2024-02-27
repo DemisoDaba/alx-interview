daeb919de378eb40750be052d2aed323776e37d2
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 15:51:27 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
 """0-making_change.py"""
-
+import sys
 def makeChange(coins, total):
     """
     MakeChange function
     """
-    if total < 0:
+    if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    res = [sys.maxsize for i in range(total + 1)]
+    res[0] = 0
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    for i in coins:
+        for j in range(i, total + 1):
 
-    return dp[total] if dp[total] != float('inf') else -1
+            res[j] = min(res[j], res[j - i] + 1)
 
+    if res[total] == sys.maxsize:
+        return -1
 
-if __name__ == "__main__":
-    makeChange = __import__('0-making_change').makeChange
-
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    return res[total]
