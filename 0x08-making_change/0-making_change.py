48400fa21e4c80a494fbbc1d9ef206ef6f6f22e7
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 20:15:24 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,16 +7,19 @@ def makeChange(coins, total):
     If total <= 0, return 0.
     If total can't be met by coins, return -1.
     """
-    if not coins or coins is None:
-        return -1
     if total <= 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
-    for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if total == 0:
-            return change
-    return -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
