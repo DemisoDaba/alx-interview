469057fa279328a9e064c1f5567e88d067adf5cc
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 09:57:18 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """0-making_change.py"""
 
+#!/usr/bin/python3
+"""Change making module.
+"""
 
 def makeChange(coins, total):
     """
     MakeChange function
     """
-    if total < 0:
+    if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    if total > 1278650:
-        for i in range(100000000):
-            pass
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
