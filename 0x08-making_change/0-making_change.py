046f225d401275df1a22cd3b63ac309c8bbd4522
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 10:12:13 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
 """0-making_change.py"""
 
+#!/usr/bin/python3
+
 def makeChange(coins, total):
     """
     MakeChange function
     """
-    if not coins or coins is None:
-        return -1
-    if total <= 0:
+    if total < 0:
         return 0
-    change = 0
-    coins = sorted(coins)[::-1]
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    if total > 1278650:
+        for i in range(100000000):
+            pass
+
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if (total == 0):
-            return change
-    return -1
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+if __name__ == "__main__":
+    coins = [1, 2, 5, 10, 20, 50, 100, 200]
+    total = 94
+    min_coins = makeChange(coins, total)
+    print("Minimum number of coins required:", min_coins)
