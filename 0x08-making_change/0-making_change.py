e5616ea8d315ebf28ebfd59033f9650ee8c2f520
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-02-27 16:00:46 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """0-making_change.py"""
-import math
+
 def makeChange(coins, total):
-    """
-    MakeChange function
-    """
-    n = 0
-    coins.sort(reverse=True)
     if total < 0:
         return 0
 
+    coins.sort(reverse=True)
+    num_coins = 0
+
     for coin in coins:
-        if total % coin <= total:
-            n += math.trunc(total / coin)
+        if coin <= total:
+            num_coins += total // coin
             total %= coin
 
-    if total == 0:
-        return n
-
-    return -1
+    return num_coins if total == 0 else -1
