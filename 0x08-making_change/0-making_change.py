5de301ec69975856655c2b727befdf6109178e79
DemisoDaba <demo.nkmt1@gmail.com>
2024-02-27 09:53:12 -0800

    Implement makeChange function for coin change calculation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,23 +7,16 @@ def makeChange(coins, total):
     If total <= 0, return 0.
     If total can't be met by coins, return -1.
     """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    newVal = total + 1
-    store = {0: 0}
-
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
-        return -1
-
-    return store[total]
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if total == 0:
+            return change
+    return -1
