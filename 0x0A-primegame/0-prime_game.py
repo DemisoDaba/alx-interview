ae74f9236b2228ec0a1527bcaca9ca7f7514facd
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-03-12 22:46:11 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,12 +10,31 @@ def isPrime(n):
             n: number to be checked
         Returns: a boolean indicating whether the number is prime or not
     """
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    if n == 1 or n == 0:
+    if n <= 1:
         return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
     return True
+
+
+def primeCountInRange(n):
+    """
+    Computes the number of prime numbers in the given range n
+
+    Return: Number of prime number's between 1 and n
+    """
+    primeCount = 0
+    for i in range(2, n + 1):
+        if isPrime(i):
+            primeCount += 1
+    return primeCount
 
 
 def isWinner(x, nums):
@@ -33,27 +52,19 @@ def isWinner(x, nums):
 
     if type(x) != int or type(nums) != list:
         return None
-    if x > len(nums) or len(nums) > x or not nums:
+    if x != len(nums) or not nums:
         return None
     if any([type(x) != int for x in nums]):
         return None
 
-    for round in range(0, x):
-        moves = 0
-        while True:
-            sets = [x for x in range(1, nums[round] + 1) if x not in previous]
-            if not any([isPrime(x) for x in sets]):
-                previous = []
-                if moves == 0 or moves % 2 == 0:
-                    benPoints += 1
-                else:
-                    mariaPoints += 1
-                break
-            if any([isPrime(x) for x in sets]):
-                n = next(x for x in sets if isPrime(x))
-                previous.append(n)
-                previous.extend([x for x in sets if x % n == 0 and x != n])
-                moves += 1
+    for round in range(x):
+        if nums[round] <= 0:
+            return None
+        if primeCountInRange(nums[round]) % 2 == 0:
+            benPoints += 1
+        else:
+            mariaPoints += 1
+
     if benPoints == 0 and mariaPoints == 0:
         return 'Ben'
     elif benPoints > mariaPoints:
