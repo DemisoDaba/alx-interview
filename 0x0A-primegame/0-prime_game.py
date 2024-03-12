adbbaaaad0e82c576daceda61480c2532dcdf131
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2024-03-12 22:49:53 +0300

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,72 +1,49 @@
 #!/usr/bin/python3
 """
-Module for a function called isWinner
+this module defines the function isWinner
 """
 
 
-def isPrime(n):
-    """
-        @params:
-            n: number to be checked
-        Returns: a boolean indicating whether the number is prime or not
-    """
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def primeNumbers(n):
+    """this function return an array of prime numbers
+    smaller or equal to n using Sieve of Eratosthenes"""
+    result = []
+    prime = [True for i in range(n+1)]
+    p = 2
+    while (p * p <= n):
+        if (prime[p] is True):
+            for i in range(p * p, n+1, p):
+                prime[i] = False
+        p += 1
+    for p in range(2, n+1):
+        if prime[p]:
+            result.append(p)
+    return result
 
 
 def isWinner(x, nums):
+    """Maria and Ben are playing a game.
+    Given a set of consecutive integers starting from 1 up to and including n,
+    they take turns choosing a prime number from the set and removing
+    that number and its multiples from the set.
+    The player that cannot make a move loses the game.
+    They play x rounds of the game,
+    where n may be different for each round.
+    Assuming Maria always goes first and both players play optimally,
+    determine who the winner of each game is.
     """
-        @params:
-            x: number of rounds
-            nums: list of numbers
-        Returns:
-            The winner between Maria and Ben
-    """
 
-    mariaPoints = 0
-    benPoints = 0
-    previous = []
-
-    if type(x) != int or type(nums) != list:
-        return None
-    if x != len(nums) or not nums:
-        return None
-    if any([type(x) != int for x in nums]):
+    if x != len(nums):
         return None
 
-    for round in range(x):
-        if nums[round] <= 0:
-            return None
-        moves = 0
-        while True:
-            sets = [x for x in range(1, nums[round] + 1) if x not in previous]
-            if not any([isPrime(x) for x in sets]):
-                previous = []
-                if moves == 0 or moves % 2 == 0:
-                    benPoints += 1
-                else:
-                    mariaPoints += 1
-                break
-            if any([isPrime(x) for x in sets]):
-                n = next(x for x in sets if isPrime(x))
-                previous.append(n)
-                previous.extend([x for x in sets if x % n == 0 and x != n])
-                moves += 1
+    Ben = 0
+    Maria = 0
 
-    if benPoints == 0 and mariaPoints == 0:
-        return 'Ben'
-    elif benPoints > mariaPoints:
-        return 'Ben'
-    elif mariaPoints > benPoints:
-        return 'Maria'
-    return None
+    for num in nums:
+        primes = primeNumbers(num)
+        if len(primes) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+
+    return "Ben" if Ben > Maria else "Maria"
